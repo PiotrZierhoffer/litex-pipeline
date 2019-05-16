bed4126d31f8bf4b0734f39a7a6fe99053f5e493
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2019-05-16 03:21:29 +0200

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#ln -s ~/buildroot buildroot
+ln -s ~/buildroot buildroot
 cd buildroot
-#git am ../*patch
-make -j`nproc` linux
+git reset --hard HEAD^
+git am ../*patch
+make -j`nproc`
 cp output/images/Image ../artifacts
+cp output/images/rootfs.cpio ../artifacts
 cp output/build/linux-5.0.14/vmlinux ../artifacts
 cd ../
 /opt/renode/tests/test.sh -r artifacts litex-linux.robot
